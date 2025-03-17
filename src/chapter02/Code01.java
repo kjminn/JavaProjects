354062b79f80bac7cbfedaed968672e565420ca4
kjminn <kjminn2001@gmail.com>
2025-03-17 11:52:31 +0900

    main Method 사용법, 연결연산자, 덧셈연산자, println, print차이점

---
+++ src/chapter02/Code01.java
@@ -0,0 +1,12 @@
+package chapter02;
+
+public class Code01 {
+    public static void main(String[] args){
+        System.out.println("AISoftware");
+        System.out.print("Kyoungju Minn\n");
+        System.out.printf("Grade 1\n");
+        System.out.println("result:" + 100 + 200); //result:100200
+        System.out.println("result:" + (100 + 200));//result:300
+        System.out.println(100 + 200 + "won");//300won
+    }
+}
